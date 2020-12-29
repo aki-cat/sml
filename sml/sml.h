3a88b204a50684d6ecf2a786d1f02daf72035266
orenjiakira <orenjiakira@gmail.com>
2020-12-29 01:00:52 -0300

    Add license note in start of all files
    Also fix header guard names

--- sml/sml.h
+++
@@ -1,9 +0,0 @@
-#ifndef SLIPPYS_MATH_LIBRARY_H
-#define SLIPPYS_MATH_LIBRARY_H
-
-#include <sml/matrix4.h>
-#include <sml/quaternion.h>
-#include <sml/transform.h>
-#include <sml/vector3.h>
-
-#endif
